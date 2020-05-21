1379ae856f2fa49cbe34ed8036bbf91f7f0e9998
joe <joe@localbox.local>
2020-05-21 17:39:49 +0100

    Registered NavigationService and added StartupView

---
+++ lib/app/router.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:veggie_go_malaysia/ui/views/startup/startup_view.dart';
+
+abstract class Routes {
+  static const startupViewRoute = '/';
+}
+
+class Router {
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch(settings.name){
+      case Routes.startupViewRoute:
+        return CupertinoPageRoute<dynamic>(
+          builder: (context) => StartupView(),
+          settings: settings,
+        );
+      default:
+        return unknownRoutePage(settings.name);
+    }
+  }
+}
+
+// borrowed from auto_route:
+// returns an error page routes with a helper message.
+PageRoute unknownRoutePage(String routeName) => MaterialPageRoute(
+      builder: (ctx) => Scaffold(
+        body: Container(
+          color: Colors.redAccent,
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: Text(
+                  routeName == "/"
+                      ? 'Initial route not found! \n did you forget to annotate your home page with @initial or @MaterialRoute(initial:true)?'
+                      : 'Route name $routeName is not found!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              OutlineButton.icon(
+                label: Text('Back'),
+                icon: Icon(Icons.arrow_back),
+                onPressed: () => Navigator.of(ctx).pop(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
