d7529b19b371dc575dc7795a83cdd64320324d3f
J Kendal <13680617+joekendal@users.noreply.github.com>
2020-05-24 18:34:31 +0100

    Update travis (#4)
    * Adds testing config
    
    * Fixes typo in travis.yml
    
    * update travis.yml secure code
    
    * Let's see if this works 🙌
    
    Co-authored-by: joe <joe@localbox.local>
    Co-authored-by: alvindrakes <alvinlohjinxian@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:logger/logger.dart';
 import 'package:veggie_go_malaysia/app/locator.dart';
+
 import 'package:veggie_go_malaysia/main.dart';
 
 void main() {
@@ -8,6 +9,7 @@ void main() {
     Logger.level = Level.info;
     setupLocator();
     await tester.pumpWidget(App());
+
     await tester.pumpAndSettle(const Duration(milliseconds: 200));
   });
 }
