f3c2b9f9ede08970b5785e0538468483982a3404
alvindrakes <alvinlohjinxian@gmail.com>
2020-05-26 16:30:29 +0800

    move datamodels to core folder, added pedantic linter

--- lib/services/database/mock_firestore.dart
+++ lib/services/database/mock_firestore.dart
@@ -1,4 +1,4 @@
-import 'package:veggie_go_malaysia/datamodels/restaurant.dart';
+import 'package:veggie_go_malaysia/core/datamodels/restaurant.dart';
 import 'package:veggie_go_malaysia/services/database/abstract_firestore.dart';
 
 class MockFirestore implements DatabaseService {
