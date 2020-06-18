4778d0085df8f23abc57141d9664c72b54b8359f
alvindrakes <alvinlohjinxian@gmail.com>
2020-06-19 00:48:34 +0800

    added localization setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,21 +12,30 @@ void main() {
   Logger.level = Level.info;
 
   setupLocator();
-  runApp(App());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en', 'US'), Locale('zh', 'CN')],
+      path: 'assets/languages',
+      useOnlyLangCode: true,
+      startLocale:
+          Locale('zh', 'CN'), // just to test whether localization is working
+      fallbackLocale: Locale('en', 'US'),
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
-      title: 'Veggie Go Malaysia',
-      localizationsDelegates: <LocalizationsDelegate<dynamic>>[
-        DefaultMaterialLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-        DefaultCupertinoLocalizations.delegate,
-      ],
+      title: 'title'.tr(),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       initialRoute: Routes.startupViewRoute,
       onGenerateRoute: Router().onGenerateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
